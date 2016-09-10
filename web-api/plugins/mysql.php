d22eb4507ab281ccb88db4d5a4cb6d90f50fdbef
Breeze <billybobserver@gmail.com>
2016-09-11 00:21:32 +0100

    MySQL API plugin update

--- web-api/plugins/mysql.php
+++ web-api/plugins/mysql.php
@@ -18,6 +18,11 @@
 			return mysql_close() != false;
 		}
 		
+		function safe_str($str)
+		{
+			return mysql_real_escape_string($str);
+		}		
+		
 		function select_single($mysql_query_string)
 		{
 			$mysql_query = mysql_query($mysql_query_string);
@@ -25,6 +30,28 @@
 			if($mysql_query != false)
 			{
 				return mysql_fetch_assoc($mysql_query);
+			}
+			
+			return false;
+		}
+		
+		function select_multiple($mysql_query_string)
+		{
+			$mysql_query = mysql_query($mysql_query_string);
+			
+			if($mysql_query != false)
+			{
+				$array = array();
+				
+				while($row = mysql_fetch_row($mysql_query))
+				{
+					if(!array_push($array, $row))
+					{
+						return false;
+					}
+				}
+				
+				return array();
 			}
 			
 			return false;
