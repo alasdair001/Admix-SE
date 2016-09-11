025f9e03e8e2d6d60d10436a1a1e73d391d481de
Breeze <billybobserver@gmail.com>
2016-09-11 03:02:25 +0100

    Example Update

--- web-api/example.php
+++ web-api/example.php
@@ -3,13 +3,13 @@
 	include('api/init.php');	
 	
 	$user_array = array(
-		'id' => 0,
+		'id' => 1,
 		'username' => 'user',
 		'email' => 'email@example.com',
-		'password' => "password",
-		'state' => 0,
-		'capes' => '[]',
-		'active_cape' => 0
+		'password' => 'password',
+		'state' => STATE_ACTIVE,
+		'capes' => '[1, 2, 3]',
+		'active_cape' => 3
 	);
 	
 	$user = UserParse::parse_single($user_array);	
